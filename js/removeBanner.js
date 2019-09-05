d4f43f76aa675cde14fb7cd89827bef52bb3f00b
UCSB <ucsb@169-231-148-34.wireless.ucsb.edu>
2019-09-04 18:24:35 -0700

    Added nick pic

--- js/removeBanner.js
+++ js/removeBanner.js
@@ -1,4 +1,4 @@
-window.onload = () => {
-   let bannerNode = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
-   bannerNode.parentNode.removeChild(bannerNode);
-}
+// window.onload = () => {
+//    let bannerNode = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
+//    bannerNode.parentNode.removeChild(bannerNode);
+// }
