dbe16b624de57414aa53ccce57705e398c58a37a
Bik <bikramn123@gmail.com>
2019-09-06 14:49:10 -0700

    Added terrence

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function main() {
 
 (function () {
    'use strict';
-   
+
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -27,12 +27,12 @@ function main() {
         }
     });
 
-    $('body').scrollspy({ 
+    $('body').scrollspy({
         target: '.navbar-default',
         offset: 80
     });
-	
-	
+
+
 	// Hide nav on click
   $(".navbar-nav li a").click(function (event) {
     // check if window is small enough so dropdown is created
@@ -46,7 +46,7 @@ function main() {
     $(window).load(function() {
         var $container = $('.portfolio-items');
         $container.isotope({
-            filter: '*',
+            filter: '.concert',
             animationOptions: {
                 duration: 750,
                 easing: 'linear',
@@ -69,11 +69,11 @@ function main() {
         });
 
     });
-	
+
   	// Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
-	});	
+	});
 
 }());
 
